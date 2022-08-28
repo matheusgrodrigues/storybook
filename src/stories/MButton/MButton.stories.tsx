dc9f428106d9cffe3d6023467742beae6d0d75ae
matheus <matheusgomes1203@hotmail.com>
2022-08-28 13:53:28 -0300

    Criando variacoes de botao

--- src/stories/MButton/MButton.stories.tsx
+++ src/stories/MButton/MButton.stories.tsx
@@ -1,9 +1,40 @@
-import MButton from "./MButton";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./MButton";
 
 export default {
-  title: "Example/MButton",
-  component: MButton,
-};
+  title: "Botôes/MButton",
+  component: Button,
+  argTypes: {
+    variant: {
+      options: [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "dark",
+        "light",
+        "link",
+        "outline-primary",
+        "outline-secondary",
+        "outline-success",
+        "outline-danger",
+        "outline-warning",
+        "outline-info",
+        "outline-dark",
+        "outline-light",
+      ],
+      control: { type: "select" },
+    },
+    onClick: { action: "Clicou" },
+  },
+} as ComponentMeta<typeof Button>;
 
-export const Primary = () => <MButton />;
-export const Danger = () => <MButton variant="danger" />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>{args.children}</Button>
+);
+
+export const MButton = Template.bind({});
+MButton.args = { variant: "primary", children: "Clique aqui", active: true };
--- src/stories/MButton/MButton.tsx
+++ src/stories/MButton/MButton.tsx
@@ -1,5 +1,3 @@
-import { Button, ButtonProps } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
-export default function MButton({variant = "primary"}: ButtonProps) {
-  return <Button variant={variant}>test</Button>;
-}
+export default Button;
